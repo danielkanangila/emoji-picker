5b08199f728dfe771782483c775c83cc0a2bc4cc
danielkanangila <daniel.kanangila@gmail.com>
2020-08-23 12:42:04 -0400

    update 2020-8-23-12-43

--- src/EmojiPicker.js
+++ src/EmojiPicker.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocalStorage } from 'react-hooks'
 import emojis from 'emojis-json'
+
 import styles from './styles.module.scss'
+import SearchIcon from './SearchIcon'
 
 const EmojiPicker = ({ onClick }) => {
   const [hovered, setHovered] = useState({ name: '', emoji: '' })
@@ -26,7 +29,12 @@ const EmojiPicker = ({ onClick }) => {
 }
 
 const Header = () => {
-  return <div className={styles.emoji_picker__header}></div>
+  return (
+    <div className={styles.emoji_picker__header}>
+      <SearchBar />
+      <Tabs />
+    </div>
+  )
 }
 const Footer = ({ name, emoji }) => {
   return (
@@ -39,7 +47,7 @@ const Footer = ({ name, emoji }) => {
 
 const EmojiList = ({ name: title, emojis, onHover, onClick }) => {
   return (
-    <div>
+    <div id={title.replace(/&|\s/g, '')}>
       <h2 className={styles.emoji_picker__title}>{title}</h2>
       <div className={styles.emoji_picker__list}>
         {emojis.map((item, index) => (
@@ -58,6 +66,66 @@ const Emoji = ({ name, emoji, onHover, onClick }) => {
       onClick={() => onClick(emoji)}
     >
       {emoji}
+    </div>
+  )
+}
+
+const Tabs = () => {
+  return (
+    <div className={styles.emoji_picker__tabs}>
+      {emojis.map((item, idx) => (
+        <TabLink
+          key={idx}
+          emoji={item.illustration}
+          title={item.name}
+          onClick={(to) => console.log(to)}
+        />
+      ))}
+    </div>
+  )
+}
+
+const TabLink = ({ title, emoji, onClick }) => {
+  const [isActive, setIsActive] = useState(false)
+  const className = styles.emoji_picker__tabs__item__active
+
+  useEffect(() => {
+    return () => {
+      setIsActive(false)
+    }
+  }, [])
+
+  const handleClick = () => {
+    setIsActive(true)
+    onClick(title.replace(/&|\s/g, ''))
+  }
+  return (
+    <div
+      onClick={handleClick}
+      className={`${styles.emoji_picker__tabs__item} ${
+        isActive ? className : ''
+      }`}
+    >
+      {emoji}
+    </div>
+  )
+}
+
+const SearchBar = () => {
+  const [query, setQuery] = useState()
+
+  return (
+    <div className={styles.emoji_picker__search}>
+      <input
+        type='text'
+        name='search'
+        value={query}
+        onChange={setQuery}
+        placeholder='Search emojis'
+      />
+      <span>
+        <SearchIcon />
+      </span>
     </div>
   )
 }
