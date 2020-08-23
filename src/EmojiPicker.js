4dd65794777aa5cba78040001993e79109ad79be
danielkanangila <daniel.kanangila@gmail.com>
2020-08-23 17:33:54 -0400

    added recent emojis used [localStorage']

--- src/EmojiPicker.js
+++ src/EmojiPicker.js
@@ -5,20 +5,42 @@ import emojis from 'emojis-json'
 import styles from './styles.module.scss'
 import SearchIcon from './SearchIcon'
 
+const parseId = (title) => {
+  return title.replace(/&|\s/g, '')
+}
+
 const EmojiPicker = ({ onClick }) => {
   const [hovered, setHovered] = useState({ name: '', emoji: '' })
+  const [recentUsedEmoji, setRecentUsedEmoji] = useLocalStorage(
+    'recentUsedEmojis',
+    {
+      name: 'Recent',
+      illustration: '🕑',
+      emojis: []
+    }
+  )
+
+  const handleClick = ({ name, emoji }) => {
+    onClick(emoji)
+    const em = recentUsedEmoji.emojis
+    em.push({ name, emoji })
+    setRecentUsedEmoji({
+      ...recentUsedEmoji,
+      emojis: em
+    })
+  }
 
   return (
     <div className={styles.emoji_picker}>
       <div className={styles.emoji_picker__main}>
-        <Header />
+        <Header emojis={[recentUsedEmoji, ...emojis]} />
         <div className={styles.emoji_picker__container}>
-          {emojis.map((item, index) => (
+          {[recentUsedEmoji, ...emojis].map((item, index) => (
             <EmojiList
               key={index}
               {...item}
               onHover={setHovered}
-              onClick={onClick}
+              onClick={handleClick}
             />
           ))}
         </div>
@@ -28,11 +50,11 @@ const EmojiPicker = ({ onClick }) => {
   )
 }
 
-const Header = () => {
+const Header = ({ emojis }) => {
   return (
     <div className={styles.emoji_picker__header}>
       <SearchBar />
-      <Tabs />
+      <Tabs emojis={emojis} />
     </div>
   )
 }
@@ -46,9 +68,17 @@ const Footer = ({ name, emoji }) => {
 }
 
 const EmojiList = ({ name: title, emojis, onHover, onClick }) => {
+  const clearStorage = () => {
+    window.localStorage.removeItem('recentUsedEmojis')
+  }
   return (
-    <div id={title.replace(/&|\s/g, '')}>
-      <h2 className={styles.emoji_picker__title}>{title}</h2>
+    <div id={parseId(title)}>
+      <div className={styles.emoji_picker__list__header}>
+        <h2 className={styles.emoji_picker__title}>{title}</h2>
+        {title === 'Recent' && (
+          <button onClick={clearStorage}>Clear All</button>
+        )}
+      </div>
       <div className={styles.emoji_picker__list}>
         {emojis.map((item, index) => (
           <Emoji key={index} {...item} onHover={onHover} onClick={onClick} />
@@ -63,17 +93,18 @@ const Emoji = ({ name, emoji, onHover, onClick }) => {
     <div
       onMouseEnter={() => onHover({ name, emoji })}
       className={styles.emoji_picker__list__item}
-      onClick={() => onClick(emoji)}
+      onClick={() => onClick({ name, emoji })}
     >
       {emoji}
     </div>
   )
 }
 
-const Tabs = () => {
+const Tabs = ({ emojis }) => {
   const [activeIndex, setIsActiveIndex] = useState(0)
   const handleClick = (to, index) => {
     setIsActiveIndex(index)
+    document.getElementById(to).scrollIntoView()
   }
   return (
     <div className={styles.emoji_picker__tabs}>
@@ -95,7 +126,7 @@ const TabLink = ({ index, isActive, title, emoji, onClick }) => {
   const activeClassName = styles.emoji_picker__tabs__item__active
 
   const handleClick = () => {
-    onClick(title.replace(/&|\s/g, ''), index)
+    onClick(parseId(title), index)
   }
   return (
     <div
